1aedeb68dfb3c80c278d7bf6993e8e800797269b
j0t4 <j0t4@j0t4.com>
2024-08-14 18:36:29 +0200

    file save and file load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,103 @@
 'use client'
-import { Tldraw } from 'tldraw'
+import {
+	Tldraw,
+	DefaultMainMenu,
+	DefaultMainMenuContent,
+	TLComponents,
+	TldrawUiMenuGroup,
+	TldrawUiMenuItem,
+	TldrawUiMenuSubmenu,
+	createTLStore,
+	getSnapshot,
+	loadSnapshot
+} from 'tldraw'
+
+
+const store = createTLStore()
+
+function CustomMainMenu() {
+
+	return (
+		<DefaultMainMenu>
+			<div style={{ backgroundColor: 'white' }}>
+				<TldrawUiMenuSubmenu id="file"
+					label="File"
+				>
+					<TldrawUiMenuGroup id="file">
+						<TldrawUiMenuItem
+							id="load"
+							label="Load File"
+							icon="external-link"
+							readonlyOk
+							onSelect={() => {
+
+								const input = document.createElement("input");
+								input.type = "file";
+								input.onchange = function (event) {
+									try {
+										let files = input.files;
+										if (files !== null) {
+											if (files.length > 0) {
+												let file = files[0];
+												if (file !== null) {
+													let reader = new FileReader();
+													const self = this;
+													reader.onload = (event) => {
+														if (event.target !== null) {
+															const result = event.target.result as string;
+															if (result !== null) {
+																loadSnapshot(store, JSON.parse(result));
+																console.log('FILE CONTENT', JSON.parse(result))
+															};
+														}
+													};
+													reader.readAsText(file);
+												}
+											}
+										}
+									} catch (err) {
+										console.error(err);
+									}
+								};
+								input.click();
+
+							}}
+						/>
+						<TldrawUiMenuItem
+							id="Save"
+							label="Save File"
+							icon="external-link"
+							readonlyOk
+							onSelect={() => {
+								const snapshot = getSnapshot(store);
+								//console.log(JSON.stringify(snapshot));
+								const link = document.createElement("a");
+								const file = new Blob([JSON.stringify(snapshot)], { type: 'text/plain' });
+								link.href = URL.createObjectURL(file);
+								link.download = "sample.tldr";
+								link.click();
+								URL.revokeObjectURL(link.href);
+							}}
+						/>
+					</TldrawUiMenuGroup>
+				</TldrawUiMenuSubmenu>
+			</div>
+			<DefaultMainMenuContent />
+		</DefaultMainMenu>
+	)
+}
+
+
+const components: TLComponents = {
+	MainMenu: CustomMainMenu,
+}
+
 
 export default function Home() {
 	return (
 		<main>
 			<div style={{ position: 'fixed', inset: 0 }}>
-				<Tldraw />
+				<Tldraw store={store} components={components} />
 			</div>
 		</main>
 	)
